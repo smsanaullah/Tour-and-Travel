80e3aafb893ac60b86ab31ca9d0ee42f487b4073
jihad08-coder <jaj016017@email.com>
2025-08-11 20:31:33 +0600

    footer file

---
+++ app/views/layout/footer.php
@@ -0,0 +1,42 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="index.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +8801815463141</a>
+            <a href="#"><i class="fas fa-phone"></i> +8801715463141</a>
+            <a href="#"><i class="fas fa-envelope"></i> chuloghurifiri@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> mohammadpur, dhaka, bangladesh - 1200</a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+    </div>
+
+    <div class="credit">created by <span>Sanaullah, Jihad, Jahid, Saim, Abid</span> || Software_Project </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="<?= BASE_URL ?>/js/script.js"></script>
+</body>
+</html>
