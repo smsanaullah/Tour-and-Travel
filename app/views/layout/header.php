bb1eed23f5daba721c80e95d3bd6239e630c5ade
jihad08-coder <jaj016017@email.com>
2025-08-11 20:19:37 +0600

    header file

---
+++ app/views/layout/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Cholo Ghurifiri</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<section class="header">
+  <a href="/index.php?url=home/index" class="logo">Cholo GhuriFiri.</a>
+  <nav class="navbar">
+    <a href="/index.php?url=home/index">Home</a>
+    <a href="/index.php?url=about/index">About</a>
+    <a href="/index.php?url=package/list">Package</a>
+
+    <a href="/index.php?url=tour/create">Make a Tour</a>
+    <a href="/index.php?url=dashboard/index" class="btn">Dashboard</a>
+    <a href="/index.php?url=auth/logout" class="btn">Logout</a>
+    <a href="/index.php?url=auth/login" class="btn">Login</a>
+    <a href="/index.php?url=auth/register" class="btn">Register</a>
+    
+  </nav>
+  <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+</body>
+</html>
