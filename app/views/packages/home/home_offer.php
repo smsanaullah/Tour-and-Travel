abaff6fa645d51f048110f5e912c42469bc277d9
Shah Family <shahfamily@Shahs-MacBook-Air.local>
2025-08-17 22:08:55 +0600

    now added rest 4 home section code together as i deleted all

---
+++ app/views/packages/home/home_offer.php
@@ -0,0 +1,7 @@
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>Enjoy exclusive discounts on travel packages and make your dream trip more affordable. Book now and save big on your next journey!</p>
+        <a href="book.php" class="btn">book now</a>
+    </div>
+</section>
