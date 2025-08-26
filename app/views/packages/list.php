fa92018b76f5b216c0880e4de5254177004f17a9
Iftiker Jahid <iftekharjahid1999@gmail.com>
2025-08-27 00:46:36 +0600

    list.php updated

---
+++ app/views/packages/list.php
@@ -0,0 +1,153 @@
+div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/cox'sbazar.jpg" alt="">
+            </div>
+                <div class="content">
+                    <h3>Cox’s Bazar</h3>
+                    <p>Cox’s Bazar, located in the southeastern part of Bangladesh, is home to the world’s longest unbroken sandy sea beach, stretching over 120 kilometers along the Bay of Bengal.</p>
+                    <a href="<?= BASE_URL ?>/index.php?url=package/view/coxsbazar" class="btn">Book Now</a>
+                </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/saint-martin.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Saint Martin's Island</h3>
+                <p>Saint Martin's Island, the only coral island in Bangladesh, is a tropical paradise known for its crystal-clear blue waters, white sandy beaches, and rich marine biodiversity. </p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/saintmartin" class="btn">Book Now</a>
+
+            </div>
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/Sundarban.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Sundarbans</h3>
+                <p>The Sundarbans, the world's largest mangrove forest, is a UNESCO World Heritage Site known for its rich biodiversity and the famous Royal Bengal Tiger.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/sundarbans" class="btn">Book Now</a>
+            </div>
+            
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/Sreemangal.jpg" alt="Srimangal">
+            </div>
+            <div class="content">
+                <h3>Srimangal</h3>
+                <p>Srimangal, the tea capital of Bangladesh, is known for its lush tea gardens and wildlife.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/srimangal" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/kuakata.jpg" alt="Kuakata">
+            </div>
+            <div class="content">
+                <h3>Kuakata</h3>
+                <p>Kuakata is the ‘Daughter of the Sea,’ famous for its panoramic sunrise and sunset views.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/kuakata" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/amiakhum.jpg" alt="Amiakhum Waterfall">
+            </div>
+            <div class="content">
+                <h3>Amiakhum Waterfall</h3>
+                <p>One of the most beautiful waterfalls in Bangladesh, located in the Bandarban hills.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/amiakhum" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/sonargaon.jpg" alt="Sonargaon">
+            </div>
+            <div class="content">
+                <h3>Sonargaon</h3>
+                <p>Sonargaon, the ancient capital of Bengal, is rich in historical architecture.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/sonargaon" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/jaflong.jpg" alt="Jaflong">
+            </div>
+            <div class="content">
+                <h3>Jaflong</h3>
+                <p>Jaflong is a picturesque hill station on the India-Bangladesh border in Sylhet.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/jaflong" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/sylhet.jpg" alt="Sylhet">
+            </div>
+            <div class="content">
+                <h3>Sylhet</h3>
+                <p>Sylhet is known for its tea gardens, hills, and the shrine of Hazrat Shah Jalal.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/sylhet" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/sajek.jpg" alt="Sajek Valley">
+            </div>
+            <div class="content">
+                <h3>Sajek Valley</h3>
+                <p>Sajek Valley, known as the "Queen of Hills," is famous for its clouds and scenic beauty.</p>
+                <a href="<?= BASE_URL ?>/index.php?url=package/view/sajek" class="btn">Book Now</a>
+            </div>
+        </div>
+        
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/img-11.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus, laborum enim</p>
+                <a href="<?= BASE_URL ?>/index.php?url=packages/view&id=adventure_tour" class="btn"> Book Now</a>
+            </div>
+        </div>
+        
+        
+        <div class="box">
+            <div class="image">
+                <img src="<?= BASE_URL ?>/images/img-12.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>adventure & tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus, laborum enim</p>
+                <a href="<?= BASE_URL ?>/index.php?url=packages/view&id=adventure_tour" class="btn"> Book Now</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="more-package"><span class="btn">More Package</span>
+    </div>
+
+
+</section>
+
+<?php include '../app/views/layout/footer.php'; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="<?= BASE_URL ?>/js/script.js"></script>
+
+</body>
+</html>
