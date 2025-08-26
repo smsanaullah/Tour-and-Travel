fd94e51be5eea59d39383e874e073ca2dbdc3817
Sanaullah <smsanaullah0099@gmail.com>
2025-08-27 00:31:41 +0600

    forgot password interface code update

--- app/views/auth/forgot.php
+++ app/views/auth/forgot.php
@@ -3,21 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Forgot Password</title>
-    <link rel="stylesheet" href="css/log.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/log.css">
 </head>
 <body>
-    <div class="auth-container">
-        <h2>Forgot Password</h2>
+<div class="auth-container">
+    <h2>Forgot Password</h2>
 
-        
-        <p id="error" style="color:red; display:none;">Error message here</p>
-        <p id="success" style="color:green; display:none;">Success message here</p>
+    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <?php if (isset($success)) echo "<p style='color:green;'>$success</p>"; ?>
 
-        
-        <form method="POST" action="index.html">
-            <input type="email" name="email" placeholder="Enter your Email" required>
-            <button type="submit">Send OTP</button>
-        </form>
-    </div>
+    <form method="POST" action="<?= BASE_URL ?>/index.php?url=auth/sendOtp">
+        <input type="email" name="email" placeholder="Enter your Email" required>
+        <button type="submit">Send OTP</button>
+    </form>
+</div>
 </body>
 </html>
